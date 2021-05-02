b36d0e6fb88ab7acef8974e9edabbdaa6fd5e6c6
Dmitriy <Dimamakarov1941@yandex.ru>
2021-05-02 15:40:13 +0300

    import bhaptic, vr libs

---
+++ Assets/libs/Bhaptics/SDK/Scripts/BhapticsConfig.cs
@@ -0,0 +1,14 @@
+﻿using Bhaptics.Tact.Unity;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "Bhaptics/Create Config", order = 1)]
+public class BhapticsConfig : ScriptableObject
+{
+    [Header("Windows Settings")]
+    public bool launchPlayerIfNotRunning = true;
+
+    [Header("Android Settings")]
+    public BhapticsAndroidManager AndroidManagerPrefab;
+
+    public bool AlwaysScanDisconnectedDevice = false;
+}
