f0d238268b36ab759c4dc6cd1eac59529d2a5731
Dmitriy <Dimamakarov1941@yandex.ru>
2021-06-01 20:26:14 +0300

    add stay in hand script

---
+++ Assets/Scripts/Colt/StayInHand.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Collections;
+using Assets.Scripts.Colt;
+using UnityEngine;
+
+public class StayInHand : MonoBehaviour
+{
+
+    Colt _colt;
+    Transform _parent;
+    private void Start()
+    {
+        _colt = gameObject.GetComponent<Colt>();
+    }
+    public void TakeInHand()
+    {
+        _colt.enabled = true;
+        // get hand transform
+        _parent = transform.parent;
+    }
+    public void DetachedFromHand()
+    {
+        transform.parent = _parent;
+        try
+        {
+            _parent.GetComponent<MeshRenderer>().enabled = false;
+        }
+        catch (System.Exception ex)
+        {
+            print("StayInHand -> There is no material in your hands");
+        }
+    }
+}
