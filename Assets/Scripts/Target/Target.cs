eabcba92ab5d4b54b629a2ef09dfc00691821320
Dmitriy <Dimamakarov1941@yandex.ru>
2021-06-10 13:13:07 +0300

    add gray effect

--- Assets/Scripts/Target/Target.cs
+++ Assets/Scripts/Target/Target.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections;
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.Rendering;
+
 interface ITarget
 {
     Tuple<bool, float> TakeDamage(float damage);
@@ -9,8 +12,13 @@ public class Target : MonoBehaviour, ITarget
 {
     [SerializeField] VRRig _VRRig;
     [SerializeField] GameObject _dieText;
-    [SerializeField] Byte _dieTextTimer=1;
+    [SerializeField] Byte _dieTextTimer = 1;
+    [SerializeField] Volume _volume;
     float health = 100;
+    private void Start()
+    {
+        _volume = GameObject.Find("Global Volume").GetComponent<Volume>();
+    }
     public Tuple<bool, float> TakeDamage(float damage)
     {
         print($"14. {gameObject} [{health}] -> take damage : {damage} -> now have {health - damage}");
@@ -26,12 +34,20 @@ public class Target : MonoBehaviour, ITarget
     {
         _dieText.SetActive(true);
         StartCoroutine(HideDieText());
+        
+        DOTween.To(() => _volume.weight, x => _volume.weight = x, 0, 1);
+
         _VRRig.head.Die();
         _VRRig.leftHand.Die();
         _VRRig.rightHand.Die();
     }
-    IEnumerator HideDieText () {
-        yield return new WaitForSeconds (_dieTextTimer);
+    IEnumerator HideDieText()
+    {
+        yield return new WaitForSeconds(_dieTextTimer);
         _dieText.SetActive(false);
+    }
+    public void Live()
+    {
+        DOTween.To(() => _volume.weight, x => _volume.weight = x, 1, 1);
     }
 }
